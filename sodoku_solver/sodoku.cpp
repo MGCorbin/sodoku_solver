630f83a9179527f388d059845a8fef48fc7a92b3
Marcus Corbin <mgc1g18@soton.ac.uk>
2020-12-22 21:08:31 +0000

    re-worked into class, no solving yet

--- sodoku_solver/sodoku.cpp
+++ sodoku_solver/sodoku.cpp
@@ -5,4 +5,91 @@
 //  Created by Marcus Corbin on 22/12/2020.
 //
 
+#include <iostream>
+#include <fstream>
+
 #include "sodoku.hpp"
+
+Sodoku::Sodoku(const int gridSize, const int miniGridSize)
+    : m_GridSize(gridSize), m_MiniGridSize(miniGridSize)
+{
+    m_Grid.resize(m_GridSize, std::vector<char>(m_GridSize, 0));
+}
+
+bool Sodoku::load(const char* inFile)
+{
+    std::fstream inStream;
+    
+    inStream.open(inFile);
+    
+    if(inStream.is_open())
+    {
+        for(int row=0; row<m_GridSize; row++)
+        {
+            for(int col=0; col<m_GridSize; col++)
+            {
+                char val;
+                inStream >> val;
+                m_Grid[row][col] = val;
+            }
+        }
+        
+        inStream.close();
+        return true;
+    }
+    return false;
+}
+
+void Sodoku::print()
+{
+    std::string divider = " ------ ------ ------ ";
+    
+    for(int row=0; row<m_Grid.size(); row++)
+    {
+        if(!(row % m_MiniGridSize))
+        {
+            std::cout << divider << std::endl;
+        }
+        
+        for(int col=0; col<m_Grid[0].size(); col++)
+        {
+            if(!(col % m_MiniGridSize))
+            {
+                std::cout << "|";
+            }
+            std::cout << m_Grid.at(row).at(col) << " ";
+        }
+        std::cout << "|" << std::endl;
+    }
+    std::cout << divider << std::endl << std::endl;
+}
+
+bool Sodoku::solve()
+{
+    return false;
+}
+
+bool Sodoku::noConflict(int row, int col, int val)
+{
+    return false;
+}
+
+bool Sodoku::checkMiniGrid(int startRow, int startCol, int val)
+{
+    return false;
+}
+
+bool Sodoku::checkRow(int row, int col, int val)
+{
+    return false;
+}
+
+bool Sodoku::checkCol(int row, int col, int val)
+{
+    return false;
+}
+
+bool Sodoku::findEmptyCell(int &row, int &col)
+{
+    return false;
+}
--- sodoku_solver/sodoku.hpp
+++ sodoku_solver/sodoku.hpp
@@ -8,6 +8,30 @@
 #ifndef sodoku_hpp
 #define sodoku_hpp
 
-#include <stdio.h>
+#include <string>
+#include <vector>
+
+class Sodoku
+{
+public:
+    explicit Sodoku(const int gridSize = 9, const int miniGridSize = 3);
+    bool load(const char *inFile);
+    void print();
+    bool solve();
+    
+private:
+    bool noConflict(int row, int col, int val);
+    bool checkMiniGrid(int startRow, int startCol, int val);
+    bool checkRow(int row, int col, int val);
+    bool checkCol(int row, int col, int val);
+    bool findEmptyCell(int &row, int &col);
+    
+private:
+    const int m_MiniGridSize;
+    const int m_GridSize;
+    std::vector<std::vector<char>> m_Grid;
+
+    
+};
 
 #endif /* sodoku_hpp */
